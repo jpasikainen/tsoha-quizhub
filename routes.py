c3ff7d2af49b921daf10e8b5c9e24cb44ab915e5
jpasikainen <jp.asikainen@pm.me>
2021-03-17 12:51:32 +0200

    Update quiz process

--- routes.py
+++ routes.py
@@ -14,41 +14,23 @@ def quiz(id):
     question_index = 0
     if request.values.get("index") != None:
         question_index = int(request.values.get("index"))
+    
+    # Save answers
+    if question_index != 0:
+        user_answer = int(request.values.get("user_answer")) - 1
+        session[str(question_index)] = user_answer
 
     # Get the question of the index
     sql = "SELECT id, question FROM questions WHERE quiz_id=:id LIMIT 1 OFFSET :question_index"
     question = db.session.execute(sql, {"id":id, "question_index":question_index}).fetchone()
-    question_id = None
+
+    answers = []
+    # Get the answers if there is a question 
     if question != None:
         question_id = question[0]
 
-    # Get the answers for the question
-    sql = "SELECT answer, correct FROM answers WHERE question_id=:question_id"
-    answers = db.session.execute(sql, {"question_id":question_id}).fetchall()
+        # Get the answers for the question
+        sql = "SELECT answer FROM answers WHERE question_id=:question_id"
+        answers = db.session.execute(sql, {"question_id":question_id}).fetchall()
 
     return render_template("quiz.html", question=question, answers=answers, index=question_index)
-
-def quizlegacy(id):
-    sql = "SELECT * FROM questions WHERE quiz_id=:id"
-    questions = db.session.execute(sql, {"id":id}).fetchall()
-    
-    index = 0
-    if request.values.get("index") != None:
-        index = int(request.values.get("index")) + 1
-
-    answers = []
-    if index < len(questions):
-        question_id = questions[index][0]
-        sql = "SELECT answer, question_id FROM answers WHERE question_id=:index"
-        answers = db.session.execute(sql, {"index":question_id}).fetchall()
-        
-        if request.values.get("user_answer") != None:
-            # Loop index starts from 1
-            user_answer = int(request.values.get("user_answer")) - 1
-            #return str(answers)
-            # Write user's answers in a cookie
-            # Overwritten in each quiz
-            # Format; question index: chosen answer
-            session[str(index - 1)] = user_answer
-    
-    return render_template("quiz.html", quiz=questions, answers=answers, index=index)
