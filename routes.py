c33e83f98eab075a58ff2ebc93cfca1240fafff9
jpasikainen <jp.asikainen@pm.me>
2021-03-23 16:26:24 +0200

    Updated quiz creation

--- routes.py
+++ routes.py
@@ -3,6 +3,9 @@ from flask import render_template, request, session, redirect
 from werkzeug.security import check_password_hash, generate_password_hash
 from db import db
 import datetime
+from flask_wtf import FlaskForm
+from wtforms import TextField, BooleanField, FieldList, FormField, SubmitField
+from wtforms.validators import DataRequired
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -93,58 +96,80 @@ def results():
 
     return render_template("results.html", title=title, correct=correct, total=total, score=score)
 
+class AnswerForm(FlaskForm):
+    answer = TextField("Answer:")
+    correct = BooleanField("Correct")
+
+class QuestionForm(FlaskForm):
+    question = TextField("Question:", validators=[DataRequired()])
+    answers = FieldList(FormField(AnswerForm), min_entries=4)
+
+class CreateQuizForm(FlaskForm):
+    title = TextField("Quiz title:", validators=[DataRequired()])
+    questions = FieldList(FormField(QuestionForm))
+    submit_button = SubmitField("Publish")
+    add_quiz_button = SubmitField("Add New Question")
+    
+    def __init__(self, *args, **kwargs):
+        questions = kwargs.pop("questions")
+        super(CreateQuizForm, self).__init__(*args, **kwargs)
+        self.questions.min_entries = questions
+        
 @app.route("/create", methods=["POST", "GET"])
 def create():
+    # User is not logged in
     if session.get("user_id") == None:
         return redirect("/")
+    
+    # Add new FieldSet if add_quiz_button was pressed and save it to a cookie
+    if request.form.get("add_quiz_button", None):
+        session["question_count"] = session.get("question_count", 1) + 1
+        # Create question_count amount of sets
+        form = CreateQuizForm(questions=session.get("question_count", 1))
+        # Add new set if necessary
+        form.questions.append_entry()
+    else:
+        form = CreateQuizForm(questions=1)
 
-    error_message = None
+    # Validate form on submit
+    # Doesn't activate if add_quiz_button was pressed because it creates a new set
+    # which is not valid
+    if form.validate_on_submit():
+        data = form.data
+        title = data["title"]
+        questions_data = data["questions"]
 
-    if request.method == "POST":
-        session["create_quiz_page"] = session.get("create_quiz_page", 1) + 1
-        session["create_quiz_pages"] = session.get("create_quiz_pages", 1) + 1
-
-        title = request.form.get("title")
-        question = request.form.get("question")
-
-        answers = []        
-        corrects = []
-        for i in range(4):
-            answers.append(request.form.get("answer_" + str(i)))
-            corrects.append(request.form.get("correct_" + str(i), False))
-        #return str(session["quiz_id"])
-        if session.get("create_quiz_id", None) == None:
-            sql = "INSERT INTO quizzes (creator_id, title) VALUES(:creator_id, :title) RETURNING id"
-            quiz_id = db.session.execute(sql, {"creator_id":session["user_id"], "title":title}).fetchone()[0]
-            db.session.flush()
-            session["create_quiz_id"] = quiz_id
-
-        sql = "INSERT INTO questions (quiz_id, question) VALUES(:quiz_id, :question) RETURNING id"
-        question_id = db.session.execute(sql, {"quiz_id":session["create_quiz_id"], "question":question}).fetchone()[0]
+        # Create quiz and mark it as published
+        sql = "INSERT INTO quizzes (creator_id, title, published) VALUES(:creator_id, :title, TRUE) RETURNING id"
+        quiz_id = db.session.execute(sql, {"creator_id":session["user_id"], "title":title}).fetchone()[0]
         db.session.flush()
-
-        for i in range(4):
-            answer = answers[i]
-            if answer == None or answer == "" or answer == " ":
-                continue
-            correct = corrects[i]
-
-            sql = "INSERT INTO answers (question_id, answer, correct) VALUES(:question_id, :answer, :correct)"
-            db.session.execute(sql, {"question_id":question_id, "answer":answer, "correct":correct})
+        
+        for entry in questions_data:
+            question = entry["question"]
+            
+            sql = "INSERT INTO questions (quiz_id, question) VALUES(:quiz_id, :question) RETURNING id"
+            question_id = db.session.execute(sql, {"quiz_id":quiz_id, "question":question}).fetchone()[0]
             db.session.flush()
+
+            for idx, choice in enumerate(entry["answers"]):
+                answer = choice["answer"]
+                if answer == "":
+                    continue
+                    
+                correct = choice["correct"]
+                # Force the first answer to be true because it's disabled and
+                # will not appear as true in data
+                if idx == 0:
+                    correct = True
+                
+                sql = "INSERT INTO answers (question_id, answer, correct) VALUES(:question_id, :answer, :correct)"
+                db.session.execute(sql, {"question_id":question_id, "answer":answer, "correct":correct})
+                db.session.flush()
+        
         db.session.commit()
+        return redirect("/")
 
-        if request.form.get("publish"):
-            sql = "UPDATE quizzes SET published=TRUE WHERE id=:quiz_id"
-            db.session.execute(sql, {"quiz_id":session["create_quiz_id"]})
-            db.session.commit()
-
-            session.pop("create_quiz_id", None)
-            session.pop("create_quiz_page", None)
-            session.pop("create_quiz_pages", None)
-            return redirect("/")
-        return render_template("create.html", title=title)
-    return render_template("create.html")
+    return render_template("create.html", form=form)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
