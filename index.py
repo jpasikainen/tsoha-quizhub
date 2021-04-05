9d81655d0a0495621f395e8b431189515d0e3d3e
jpasikainen <jp.asikainen@pm.me>
2021-04-05 12:05:40 +0300

    Updated posts to displays days since creation

--- index.py
+++ index.py
@@ -12,7 +12,7 @@ def admin_delete_quiz(quiz_id):
     db.session.commit()
 
 def get_all_visible_quizzes():
-    sql = "SELECT users.username, quizzes.id, quizzes.title, quizzes.date, quizzes.upvotes, quizzes.downvotes " \
+    sql = "SELECT users.username, quizzes.id, quizzes.title, (date_trunc('second', NOW()::TIMESTAMP)::date - quizzes.date::date), quizzes.upvotes, quizzes.downvotes " \
         "FROM users INNER JOIN quizzes ON users.id = quizzes.creator_id " \
         "WHERE visible=TRUE AND published=TRUE AND users.removed=FALSE " \
         "ORDER BY date DESC"
